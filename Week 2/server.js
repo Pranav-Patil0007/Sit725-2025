83d6b83b2f16460cf69d708568c93ad1c7dd856c
Pranav_Patil_0007 <s225118287@deakin.edu.au>
2025-07-25 12:25:21 +1000

    2.2 P Submission

--- Week 2/server.js
+++ Week 2/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
 console.log("App listening to: "+PORT)
 });
@@ -11,16 +11,20 @@ app.use(express.json());
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 // In-memory array to store quotes
-let quotes = [
-"The best way to predict the future is to invent it.",
-"Life is 10% what happens to us and 90% how we react to it.",
-"The only limit to our realization of tomorrow is our doubts of today.",
-"Do not wait to strike till the iron is hot; but make it hot by striking."
-];
-// GET endpoint to retrieve a random quote
-// Usage example: http://localhost:3000/api/quote
-app.get('/api/quote', (req, res) => {
-const randomIndex = Math.floor(Math.random() * quotes.length);
-res.json({ quote: quotes[randomIndex] });
+
+
+app.get('/res',(req,res) => {
+
+    const a = parseFloat(req.query.a);
+    const b = parseFloat(req.query.b);
+    const o = req.query.o;
+    var tex = a + o + b;
+    if(isNaN(a) || isNaN(b))
+    {
+        return res.send("Error: Please enter two valid numbers in 'a' and 'b'.");
+    }
+
+    const resu = eval(tex);
+    res.send(`The result of ${a} ${o} ${b} = ${resu}`);
+
 });
-// Optional: POST endpoint to add a new quote
