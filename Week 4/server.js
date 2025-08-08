f964b7cd528a8457f925091ae2788b561d24ab21
Pranav_Patil_0007 <s225118287@deakin.edu.au>
2025-08-08 22:46:43 +1000

    Task 4.2 P

--- Week 4/server.js
+++ Week 4/server.js
@@ -31,3 +31,12 @@ app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
 
+const sampleProject = new Project({
+title: "Kitten 4",
+image: "images/kitten-4.jpeg",
+link: "About Kitten 4",
+description: "Demo description about kitten 4"
+});
+sampleProject.save().then(() => console.log("Sample project saved!"));
+
+
